6e93edcaef2c1af369c0d60b0b38856510554a4b
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-01 19:37:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,11 +38,9 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   get_user_input
-  if get_user_input == "h"
+  get_user_input == "h"
     deal_card
-  else
     return card_total
-  end
 end
 
 def invalid_command
