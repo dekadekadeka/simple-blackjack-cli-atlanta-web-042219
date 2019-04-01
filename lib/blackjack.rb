6b33555bd41295d4ea90d44ab9fdfadaafeaa8db
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-01 20:20:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,6 +58,9 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  hit?(card_total)
-  display_card_total(card_total)
+  until card_total == 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+end_game
 end
